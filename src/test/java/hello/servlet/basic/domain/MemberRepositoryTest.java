a478c7f224848e37e1c56c1e29cf69303b352339
gijoongjang <rlwnd0000@naver.com>
2022-11-16 22:02:04 +0900

    Initial Commit

---
+++ src/test/java/hello/servlet/basic/domain/MemberRepositoryTest.java
@@ -0,0 +1,49 @@
+package hello.servlet.basic.domain;
+
+import hello.servlet.domain.Member;
+import hello.servlet.domain.MemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MemberRepositoryTest {
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @AfterEach
+    void afterEach() {
+        memberRepository.clearStore();
+    }
+
+    @Test
+    void 저장() {
+        //given
+        Member member = new Member("hello", 20);
+
+        //when
+        Member savedMember = memberRepository.save(member);
+
+        //then
+        Member findMember = memberRepository.findById(savedMember.getId());
+        assertThat(findMember).isEqualTo(savedMember);
+    }
+
+    @Test
+    void 전체_불러오기() {
+        //given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 23);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        //when
+        List<Member> result = memberRepository.findAll();
+
+        //then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1, member2);
+    }
+}
