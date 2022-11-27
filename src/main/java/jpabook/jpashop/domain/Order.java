0a79377a31739f80cf0898ed4f3ea3377bb25d31
gijoongjang <rlwnd0000@naver.com>
2022-11-27 12:36:18 +0900

    주문 레파지토리 추가

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Entity
@@ -56,9 +57,7 @@ public class Order {
         Order order = new Order();
         order.setMember(member);
         order.setDelivery(delivery);
-        for(OrderItem orderItem : orderItems) {
-            order.addOrderItem(orderItem);
-        }
+        Arrays.stream(orderItems).forEach(order::addOrderItem);
 
         order.setStatus(OrderStatus.ORDER);
         order.setOrderDate(LocalDateTime.now());
@@ -73,9 +72,7 @@ public class Order {
         }
 
         this.setStatus(OrderStatus.CANCEL);
-        for(OrderItem orderItem : orderItems) {
-            orderItem.cancel();
-        }
+        orderItems.forEach(OrderItem::cancel);
     }
 
     //==조회 로직==//
