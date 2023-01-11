bbdb16e6d40d592cd11f1aac87dbfbe10a2bfa12
gijoongjang <rlwnd0000@naver.com>
2023-01-11 19:52:53 +0900

    벌크성 수정 쿼리(영속성 컨텍스트 초기화 관련)

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -16,6 +16,8 @@ import study.datajpa.entity.Member;
 import study.datajpa.entity.Team;
 
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -33,6 +35,9 @@ class MemberRepositoryTest {
 
     @Autowired
     TeamRepository teamRepository;
+
+    @PersistenceContext
+    EntityManager em;
 
     @Test
     @DisplayName("멤버 테스트")
@@ -175,5 +180,23 @@ class MemberRepositoryTest {
         assertThat(page.getTotalPages()).isEqualTo(2);
         assertThat(page.isFirst()).isTrue();
         assertThat(page.hasNext()).isTrue();
+    }
+
+    @Test
+    public void bulkUpdate() {
+        memberRepository.save(new Member("member1", 10));
+        memberRepository.save(new Member("member2", 19));
+        memberRepository.save(new Member("member3", 20));
+        memberRepository.save(new Member("member4", 21));
+        memberRepository.save(new Member("member5", 40));
+
+        int resultCount = memberRepository.bulkAgePlus(20);
+//        em.flush();
+//        em.clear();
+
+        Optional<Member> member5 = memberRepository.findOptionalByUsername("member5");
+        System.out.println("member5 = " + member5.get());
+
+        assertThat(resultCount).isEqualTo(3);
     }
 }
