c293675bd295c0b0270417f4d8459ff743411880
gijoongjang <rlwnd0000@naver.com>
2023-01-28 14:33:40 +0900

    JPQL 과 Querydsl 차이점 예제 추가

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Hello;
-import study.entity.QHello;
+import study.querydsl.entity.QHello;
 
 import javax.persistence.EntityManager;
 
